10071cd7a0207f6edd9ab466c01c256629e4cad1
claudegose <84382636+claudegose@users.noreply.github.com>
2021-06-11 11:08:26 +0200

    Color Change Button With JS Function Complete and Operational

--- 02-colors/01-change-bcg-one/script.js
+++ 02-colors/01-change-bcg-one/script.js
@@ -9,8 +9,23 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+function black() {
+    document.body.style.backgroundColor="black"
+}
 
-    // your code here
+function red() {
+    document.body.style.backgroundColor="red"
+}
 
-})();
+function green() {
+    document.body.style.backgroundColor="green"
+}
+
+function yellow() {
+    document.body.style.backgroundColor="yellow"
+}
+
+function blue() {
+    document.body.style.backgroundColor="blue"
+}
+
