6fde2ab2201f5c9d3d72301818c49e92a6808da4
claudegose <84382636+claudegose@users.noreply.github.com>
2021-06-13 15:10:38 +0200

    Done

--- 03-dates/04-locale-date-Optional/script.js
+++ 03-dates/04-locale-date-Optional/script.js
@@ -9,10 +9,18 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
-    // your code here
-
-})();
+function formatAMPM() {
+    var date = new Date();
+    var hours = date.getHours();
+    var days = date.getDay();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = date + ' ' + hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+document.getElementById("thedate").innerHTML = formatAMPM();
