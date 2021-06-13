83bfc452e8221156bb459902a7edf1f5cfcba0c2
claudegose <84382636+claudegose@users.noreply.github.com>
2021-06-13 14:39:16 +0200

    02 Greet Function Complete

--- 03-dates/02-title-by-hour-two/script.js
+++ 03-dates/02-title-by-hour-two/script.js
@@ -9,10 +9,16 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
-    // your code here
+var myDate = new Date();
+var hrs = myDate.getHours();
 
-})();
+var greet;
+
+if(hrs < 1730)
+    greet = 'Hello';
+else if(hrs >= 1730)
+     greet = 'Good Evening';
+
+ document.getElementById("changeMe").innerHTML = greet
