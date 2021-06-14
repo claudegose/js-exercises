cfbf00dd9f37ede7f7e7895459014e2c72faedc0
claudegose <84382636+claudegose@users.noreply.github.com>
2021-06-14 16:35:21 +0200

    FIZ BUZZ

--- 04-maths/03-fizzbuzz/script.js
+++ 04-maths/03-fizzbuzz/script.js
@@ -1,16 +1,21 @@
-/* becode/javascript
- *
- * /02-maths/06-fizzbuzz/script.js - 2.6: fizzbuzz
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
-(function() {
 
-    // your code here
 
-})();
+function number () {
+    for (let i = 1; i < 100; i++) {
+        if (i % 3) {
+            console.log("FIZZ");
+        } else if (i % 5) {
+            console.log("BUZZ");
+        } else if (i % 15) {
+            console.log("FIZZBUZZ");
+        } else {
+            console.log(i);
+        }
+    }
+
+}
+
+
+number(3);
