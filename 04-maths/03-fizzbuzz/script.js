f83b18cef16efeaedd8ba75cf413cb6597b72bbc
claudegose <84382636+claudegose@users.noreply.github.com>
2021-06-14 21:52:48 +0200

    FIZZBUZZ Complete

--- 04-maths/03-fizzbuzz/script.js
+++ 04-maths/03-fizzbuzz/script.js
@@ -1,21 +1,32 @@
 
 
 
+// for loop
 
-function number () {
-    for (let i = 1; i < 100; i++) {
-        if (i % 3) {
-            console.log("FIZZ");
-        } else if (i % 5) {
-            console.log("BUZZ");
-        } else if (i % 15) {
-            console.log("FIZZBUZZ");
-        } else {
-            console.log(i);
-        }
+for (i = 1; i < 100; i++){
+    if ( i % 3 === 0){
+        console.log('FIZZ');
+    } else if ( i % 5 === 0){
+        console.log('BUZZ')
+    } else if ( i % 15 === 0){
+        console.log('FIZZBUZZ');
+    } else {
+        console.log(i);
     }
-
 }
 
 
-number(3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
