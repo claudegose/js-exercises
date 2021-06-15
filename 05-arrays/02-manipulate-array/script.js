b7925b3554af13d473e895f1d86ce4437678b029
claudegose <84382636+claudegose@users.noreply.github.com>
2021-06-15 18:32:15 +0200

    Manipulate Array Complete

--- 05-arrays/02-manipulate-array/script.js
+++ 05-arrays/02-manipulate-array/script.js
@@ -1,29 +1,17 @@
-/* becode/javascript
- *
- * /05-arrays/02-manipulate-array/script.js - 5.2: manipulation de tableau
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
+var fruits = ["pomme", "poire", "fraise", "tomate", "orange", "mandarine", "durian", "pêche", "raisin", "cerise"];
 
-(function() {
 
-    var fruits = [
-        "pomme",
-        "poire",
-        "fraise",
-        "tomate",
-        "orange",
-        "mandarine",
-        "durian",
-        "pêche",
-        "raisin",
-        "cerise",
-    ];
+function newClick () {
+    fruits.shift();
+    fruits.splice(9,1);
+    fruits.unshift("banana");
+    fruits.push("kiwi");
+    console.log(fruits);
+}
 
-    // your code here
 
-})();
+
+
+
+
