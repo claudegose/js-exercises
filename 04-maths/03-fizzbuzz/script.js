8fb98f34d02dc3e963b6d041c5b2994e456d543b
claudegose <84382636+claudegose@users.noreply.github.com>
2021-06-15 12:27:14 +0200

    FizzBuzz Bug FIX

--- 04-maths/03-fizzbuzz/script.js
+++ 04-maths/03-fizzbuzz/script.js
@@ -1,18 +1,16 @@
 
 
 
-// for loop
-
-for (i = 1; i < 100; i++){
-    if ( i % 3 === 0){
-        console.log('FIZZ');
-    } else if ( i % 5 === 0){
-        console.log('BUZZ')
-    } else if ( i % 15 === 0){
-        console.log('FIZZBUZZ');
-    } else {
-        console.log(i);
-    }
+for (n = 1; n < 100; n++){
+if ( n % 3 === 0){
+    console.log('FIZZ');
+} else if (n % 5 === 0){
+    console.log('BUZZ');
+} else if (n % 15 === 0) {
+    console.log('FIZZBUZZ');
+} else{
+        console.log(n)
+    };
 }
 
 
