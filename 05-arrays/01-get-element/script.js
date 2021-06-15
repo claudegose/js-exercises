3467c3594f2bff1ddcadaf5246682073a7de8214
claudegose <84382636+claudegose@users.noreply.github.com>
2021-06-15 14:50:40 +0200

    1st Array Chapter 5 Complete

--- 05-arrays/01-get-element/script.js
+++ 05-arrays/01-get-element/script.js
@@ -1,16 +1,7 @@
-/* becode/javascript
- *
- * /05-arrays/01-get-element/script.js - 5.1: élément de tableau
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 const fruits = ['apple', 'pear', 'raspberry', 'tomatos', 'kiwi', 'banana', 'orange', 'mandarin', 'durian', 'peach', 'grape', 'cherry'];
 
 function runTime () {
     console.log(fruits[4])
+
 }
