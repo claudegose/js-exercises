0b64a74b655bd26519f7d1764d6a115e3f71fc7e
claudegose <84382636+claudegose@users.noreply.github.com>
2021-06-15 12:38:03 +0200

    Repracticed Functions

--- 04-maths/01-calculator-one/script.js
+++ 04-maths/01-calculator-one/script.js
@@ -1,26 +1,23 @@
-function firstOne () {
-    var a = parseInt(document.getElementById("number1").value);
-    var b = parseInt(document.getElementById("number2").value);
-    document.getElementById("result").innerHTML = (a + b);
+function firstOne (){
+    let a = parseInt(document.getElementById("number1").value);
+    let b = parseInt(document.getElementById("number2").value);
+    document.getElementById("results").innerHTML = (a + b);
 
+
+}function secondOne() {
+    let a = parseInt(document.getElementById("number1").value);
+    let b = parseInt(document.getElementById("number2").value);
+    document.getElementById("results").innerHTML = (a - b);
 }
-
-
-function secondOne () {
-    var a = parseInt(document.getElementById("number1").value);
-    var b = parseInt(document.getElementById("number2").value);
-    document.getElementById("result").innerHTML = ( a - b);
-}
-
 
 function thirdOne () {
-    var a = parseInt(document.getElementById("number1").value);
-    var b = parseInt(document.getElementById("number2").value);
-    document.getElementById("result").innerHTML = (a * b);
+    let a = parseInt(document.getElementById("number1").value);
+    let b = parseInt(document.getElementById("number2").value);
+    document.getElementById("results").innerHTML = (a * b);
 }
 
 function fourthOne () {
-    var a = parseInt(document.getElementById("number1").value);
-    var b = parseInt(document.getElementById("number2").value);
-    document.getElementById("result").innerHTML = (a / b );
+    let a = parseInt(document.getElementById("number1").value);
+    let b = parseInt(document.getElementById("number2").value);
+    document.getElementById("results").innerHTML = (a / b);
 }
