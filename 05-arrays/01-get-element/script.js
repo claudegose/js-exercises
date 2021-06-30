5ee7710579b81b3e566d34cccde97598340cb673
Claude Gose <84382636+claudegose@users.noreply.github.com>
2021-06-30 10:44:47 +0200

    Delete 05-arrays directory

--- 05-arrays/01-get-element/script.js
+++
@@ -1,8 +0,0 @@
-
-const fruits = ['apple', 'pear', 'raspberry', 'tomatos', 'kiwi', 'banana', 'orange', 'mandarin', 'durian', 'peach', 'grape', 'cherry'];
-
-
-
-document.getElementById("run").addEventListener("click", () => {
-    console.log(fruits[3])
-})
